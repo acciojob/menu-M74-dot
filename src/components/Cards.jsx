6d4c37f2b5415c28c7ad97573838183821cc452d
manishamali1525 <manishamali1525@users.noreply.github.com>
2024-09-20 09:20:07 +0530

    Updated files

---
+++ src/components/Cards.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Cards = ({img,title,price,desc}) => {
+    return(
+        <div style={{display:"flex",width:"450px",height:"150px",border:"1px solid black",marginTop:"30px"}}>
+            <div style={{padding:"20px"}}>
+                <img src={img} alt="image-borken"/>
+            </div>
+            <div>
+                <div style={{display:"flex",justifyContent:"space-around",padding:"10px"}}>
+                    <span style={{fontSize:"18px",fontWeight:"bold"}}>{title}</span>
+                    <span style={{color:"orange"}}>${price}</span>
+                </div>
+                <div style={{textAlign:"justify",padding:"10px"}}>
+                    {desc}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Cards;
